949f623b77ebaac3b37c338ef0ac5d0ab1456ded
Prabin K C <pkmiracle36@gmail.com>
2024-07-04 12:36:50 +1000

    using Alert component to be triggered when clicking the button

--- src/app/alert/page.tsx
+++ src/app/alert/page.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Alert from '../components/Alert'
 
 export default function page() {
+const [alerts, setAlerts] = useState<{id:number, alert: React.JSX.Element}[]>([])
+const addAlert =() => {
+    const updatedAlertsArray = [...alerts, {id: Date.now(), alert: <Alert />}]
+    setAlerts(updatedAlertsArray)
+}
   return (
     <>
-    <Alert />
+    {/* alerts container */}
+    <div className='flex flex-col justify-between items-start gap-2 m-2'>
+        {/* alerts will appear here and also get stacked */}
+        {alerts.map((item, index) => (
+            <Alert key={index}/>
+        ))}
+        {/* button to trigger alerts */}
+    </div>
+    <button onClick={() => addAlert()}className='border-4 border-blue-300 rounded-md p-2 bg-blue-400 flex justify-center items-center text-white'>click to trigger alerts</button>
     </>
   )
 }
